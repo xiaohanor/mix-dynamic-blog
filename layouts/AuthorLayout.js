8d577b149729d686563929f776ab33dc27081f8d
Lumoon <36807573+xiaohanor@users.noreply.github.com>
2023-04-28 18:52:51 +0800

    Update AuthorLayout.js

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -29,7 +29,6 @@ export default function AuthorLayout({ children, frontMatter }) {
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
               <SocialIcon kind="twitter" href={twitter} />
               <SocialIcon kind="bilibili" href={bilibili} />
             </div>
