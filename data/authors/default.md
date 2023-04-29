b03794815ab19726eef162f26246baecebec1efe
Lumoon <36807573+xiaohanor@users.noreply.github.com>
2023-04-29 15:58:01 +0800

    测试

--- data/authors/default.md
+++ data/authors/default.md
@@ -3,10 +3,10 @@ name: Lain
 avatar: /static/images/avatar.png
 occupation: CEO
 company: Mix Dynamic
-email: address@yoursite.com
+email: xiaohanor@qq.com
 twitter: https://twitter.com/Twitter
-github: https://github.com
-bilibili: https://space.bilibili.com/123456
+github: https://github.com/xiaohanor
+bilibili: https://space.bilibili.com/351233837
 ---
 
-这里是混合动力Mix Dynamic, 交流游戏，影视，科技的一切。 [ 这里没有答案，只有热爱 ]
+
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -39,10 +39,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: process.env.xiaohanor/mix-dynamic-blog,
+      repositoryId: process.env.R_kgDOJcaWaQ,
+      category: process.env.Announcements,
+      categoryId: process.env.DIC_kwDOJcaWac4CWHaM,
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
