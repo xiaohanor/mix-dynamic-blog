d663656c8aeb15391567e8b2727626959463a47f
Lumoon <36807573+xiaohanor@users.noreply.github.com>
2023-04-29 16:07:52 +0800

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -39,10 +39,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.xiaohanor/mix-dynamic-blog,
-      repositoryId: process.env.R_kgDOJcaWaQ,
-      category: process.env.Announcements,
-      categoryId: process.env.DIC_kwDOJcaWac4CWHaM,
+      repo: xiaohanor/mix-dynamic-blog,
+      repositoryId: R_kgDOJcaWaQ,
+      category: Announcements,
+      categoryId: DIC_kwDOJcaWac4CWHaM,
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
