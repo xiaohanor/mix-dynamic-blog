a589d8b36ee9e70b499a6f9af7336de72496a22e
Lumoon <36807573+xiaohanor@users.noreply.github.com>
2023-07-07 18:06:43 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,16 @@
-import '@/css/tailwind.css'
-import '@/css/prism.css'
-import 'katex/dist/katex.css'
-
-import '@fontsource/inter/variable-full.css'
-
-import { ThemeProvider } from 'next-themes'
-import Head from 'next/head'
-
-import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
-import LayoutWrapper from '@/components/LayoutWrapper'
-import { ClientReload } from '@/components/ClientReload'
 import { Analytics } from '@vercel/analytics/react';
+import '@/css/tailwind.css';
+import '@/css/prism.css';
+import 'katex/dist/katex.css';
+import '@fontsource/inter/variable-full.css';
+import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
+import siteMetadata from '@/data/siteMetadata';
+import LayoutWrapper from '@/components/LayoutWrapper';
+import { ClientReload } from '@/components/ClientReload';
 
-const isDevelopment = process.env.NODE_ENV === 'development'
-const isSocket = process.env.SOCKET
+const isDevelopment = process.env.NODE_ENV === 'development';
+const isSocket = process.env.SOCKET;
 
 export default function App({ Component, pageProps }) {
   return (
@@ -23,10 +19,10 @@ export default function App({ Component, pageProps }) {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
+      <Analytics />
     </ThemeProvider>
-  )
+  );
 }
